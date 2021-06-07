ea5bd9e2e3043692e52a90e28337625f076a8d23
Lew Wai Cheong <lwc_Op.27@hotmail.com>
2021-06-07 17:52:42 +0800

    DDPG algo update

--- agent.py
+++ agent.py
@@ -2,15 +2,33 @@ import torch
 import torch.nn as nn
 import torch.functional as F
 
+from models import *
+
 class DDPG_agent():
     """This class object design  the ddpg agent
-    :param
-
-
     """
+    def __init__(self, state_size, action_size, ddpg_body_dim, seed, batch_Sizse, tau, decay_noise):
+        """INitialise an Agent
+        :param
+            state_siize (int): Dimensions of each states; (n_agents, dims) - (2,24)
+            action_size (int): Dimensions of each action; (n_agents, act_dim) - (2,2)
+            seed (int): random seed
+            batch_size (int): size of each samples
+            tau (float): Learning rate of the
+            decay_noise (float): OUIS noise will decay with each time step
 
-    # Actor
-    self.actor_local = 
+        """
+
+        # Actor and critic network
+        self.network_local = DDPGmodel(state_size=state_size, action_size=action_size,
+                                       ddpgActor_body_dim=ddpg_body_dim[0], ddpgCritic_body_dim=ddpg_body_dim[1],
+                                       seed=seed)
+        self.network_target = DDPGmodel(state_size=state_size, action_size=action_size,
+                                        ddpgActor_body_dim=ddpg_body_dim[0], ddpgCritic_body_dim=ddpg_body_dim[1],
+                                        seed=seed)
+        self.network_target.load_state_dict(self.network_local.state_dict())
 
 
-    # Critic
+    
+
+
--- models.py
+++ models.py
@@ -4,9 +4,57 @@ import torch.functional as F
 
 
 def swish(x):
-    return x*torch.sigmoid(x)
+    return x * torch.sigmoid(x)
 
+def uniform_init(layer):
+    nn.init.uniform_(layer.weight.data)
+    nn.init.constant_(layer.bias.data, 0)
+    return layer
 
-class DDPGmodel(nn.module):
+# placeholder for ddpg dimensions
+ddpgActor_body_dim = (256, 128)
+ddpgCritic_body_dim = (256, 128)
+
+# States = (n_agents, states_dim)
+# actions = (n_agents, actions_len)
+class DDPGmodel(nn.Module):
     """Model designed for DDPG"""
-    def __init__(self):
+
+    def __init__(self, state_size, action_size, ddpgActor_body_dim, ddpgCritic_body_dim, seed):
+        super(DDPGmodel, self).__init__()
+        """
+        Initialise DDPG model for DDPG algorithm, consist of both actor and crictic method"""
+        self.seed = torch.manual_seed(seed)
+
+        self.state_size = state_size
+        self.action_size = action_size
+        self.ddpgActor_dim = (state_size,) + ddpgActor_body_dim
+        self.ddpgCritic_dim = (state_size + action_size,) + ddpgCritic_body_dim
+
+        # Actor
+        self.actor = nn.ModuleList(
+            [uniform_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in zip(self.ddpgActor_dim[:-1], self.ddpgActor_dim[1:])]
+        )
+        self.actor_feature_dim = self.ddpgActor_dim[-1]
+        self.actor_fc = uniform_init(nn.Linear(self.actor_feature_dim, self.action_size))
+
+        # Critic
+        self.critic = nn.ModuleList(
+            [uniform_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in zip(ddpgCritic_dim[:-1], ddpgCritic_dim[1:])]
+        )
+        self.critic_feature_dim = self.ddpgCritic_dim[-1]
+        self.critic_fc = uniform_init(nn.Linear(self.critic_feature_dim,1))
+
+    def forward(self, x):
+        """Forward action here"""
+        for layer in self.actor:
+            x = swish(layer(x))
+        action = torch.tanh(self)
+        return action
+
+    def critic_forward(self, obs_act):
+        """Forward critic networ"""
+        for layer in self.critic:
+            obs_act = swish(obs_act)
+        V = self.critic_fc(obs_act)
+        return V
