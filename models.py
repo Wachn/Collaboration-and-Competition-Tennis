60f6504776b60fff9d6c481f285c8374b91fcef1
Lew Wai Cheong <lwc_Op.27@hotmail.com>
2021-06-25 15:53:50 +0800

    Extension to v1.0

--- models.py
+++ models.py
@@ -7,21 +7,25 @@ import torch.optim as optim
 def swish(x):
     return x * torch.sigmoid(x)
 
+
 def uniform_init(layer):
     nn.init.uniform_(layer.weight.data)
     nn.init.constant_(layer.bias.data, 0)
     return layer
 
+
 # placeholder for ddpg dimensions
 ddpgActor_body_dim = (256, 128)
 ddpgCritic_body_dim = (256, 128)
+
 
 # States = (n_agents, states_dim)
 # actions = (n_agents, actions_len)
 class DDPGmodel(nn.Module):
     """Model designed for DDPG"""
 
-    def __init__(self, state_size, action_size, ddpgActor_body_dim, ddpgCritic_body_dim, seed, lr_actor, lr_critic, weight_decay):
+    def __init__(self, state_size, action_size, ddpgActor_body_dim, ddpgCritic_body_dim, seed, lr_actor, lr_critic,
+                 weight_decay):
         super(DDPGmodel, self).__init__()
         """
         Initialise DDPG model for DDPG algorithm, consist of both actor and crictic method"""
@@ -30,26 +34,27 @@ class DDPGmodel(nn.Module):
         self.state_size = state_size
         self.action_size = action_size
         self.ddpgActor_dim = (state_size,) + ddpgActor_body_dim
-        self.ddpgCritic_dim = (state_size + 2*action_size,) + ddpgCritic_body_dim
+        self.ddpgCritic_dim = (state_size + 2 * action_size,) + ddpgCritic_body_dim
 
         # Actor
         self.actor = nn.ModuleList(
-            [uniform_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in zip(self.ddpgActor_dim[:-1], self.ddpgActor_dim[1:])]
+            [uniform_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in
+             zip(self.ddpgActor_dim[:-1], self.ddpgActor_dim[1:])]
         )
         self.actor_feature_dim = self.ddpgActor_dim[-1]
         self.actor_fc = uniform_init(nn.Linear(self.actor_feature_dim, self.action_size))
 
         # Critic
         self.critic = nn.ModuleList(
-            [uniform_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in zip(self.ddpgCritic_dim[:-1], self.ddpgCritic_dim[1:])]
+            [uniform_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in
+             zip(self.ddpgCritic_dim[:-1], self.ddpgCritic_dim[1:])]
         )
         self.critic_feature_dim = self.ddpgCritic_dim[-1]
-        self.critic_fc = uniform_init(nn.Linear(self.critic_feature_dim,1))
-
+        self.critic_fc = uniform_init(nn.Linear(self.critic_feature_dim, 1))
 
         # Parameters for backpropagation
         self.actor_params = list(self.actor.parameters()) + list(self.actor_fc.parameters())
-        self.critic_params = list(self.critic.parameters()) + list (self.critic_fc.parameters())
+        self.critic_params = list(self.critic.parameters()) + list(self.critic_fc.parameters())
 
         self.optim_actor = optim.Adam(self.actor_params, lr=lr_actor, weight_decay=weight_decay)
         self.optim_critic = optim.Adam(self.critic_params, lr=lr_critic, weight_decay=weight_decay)
@@ -67,3 +72,5 @@ class DDPGmodel(nn.Module):
             obs_act = swish(layer(obs_act))
         V = self.critic_fc(obs_act)
         return V
+
+
